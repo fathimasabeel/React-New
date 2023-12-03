0cae70488bd84272404afb1543e0303e4bc7d023
fathimasabeel <fathimasabeel97@gmail.com>
2023-12-03 17:08:50 +0530

    Add ExpenseItem.js file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,32 @@
-function ExpenseItem(){
+import './ExpenseItem.css';
+
+function ExpenseItem(props){
+    // const expenseDate = new Date(2021, 2, 28)
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 294.67;
+    // const title = 'Expense item';
+    // const locationOfExpenditure = 'Supermarket'; 
+
     return (
-        <div>
-            <h2>Expense item</h2>
-            <div>Food Rs 10</div>
-            <div>Petrol Rs 100</div>
-            <div>Movies Rs 200</div>
-        </div>
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div>
+                {/* other JSX code */}
+                <div>Location: {props.locationOfExpenditure}</div>
+            </div>
+            <div>
+                {/* <div>Food Rs 10</div>
+                <div>Petrol Rs 100</div>
+                <div>Movies Rs 200</div> */}
+
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">${props.amount}</div>
+                </div>
+            </div>
+        </div>    
     )
 }
 
 export default ExpenseItem;
+
