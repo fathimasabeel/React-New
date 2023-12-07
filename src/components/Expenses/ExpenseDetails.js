5255587d9f2f917345384b854dbb21ff7a40a37f
fathimasabeel <fathimasabeel97@gmail.com>
2023-12-07 19:18:21 +0530

    Add ExpenseDetails.js file

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,19 +1,24 @@
+import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
 import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
+
 
 const ExpenseDetails = (props) => {
+    const clickHandler = () => {
+        console.log('Expense deleted!!!');
+        // Remove the expense from the DOM
+        const removeExpense = document.querySelector('.expense-item');
+        removeExpense.remove();
+    }
     return (
         <Card className="expense-item">
-            {/* <ExpenseDate date ={props.date} /> */}
             <div className="expense-item__description">
-                <ExpenseDate date ={props.date} />
-
-                <div>Location: {props.locationOfExpenditure}</div>
+                <ExpenseDate date={props.date} />
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
+                <button onClick={clickHandler}>Delete Expense</button>
             </div>
-        </Card>    
+        </Card>
     )  
 }
 export default ExpenseDetails;
