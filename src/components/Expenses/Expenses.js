877c3870bca07899cbdbf79ed875cf662f1c0e5e
fathimasabeel <fathimasabeel97@gmail.com>
2023-12-12 19:28:53 +0530

    Add Expenses.js file

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,42 +11,27 @@ const Expenses = (props) => {
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear)
     }
+    const filterExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <div>
             <Card className="expenses">  
                 <ExpensesFilter 
                     selected={filteredYear} 
                     onChangeFilter={filterChangeHandler}
-                />
-                {props.items.map((expense) => (
-                    <ExpenseDetails
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                        // locationOfExpenditure={expense.locationOfExpenditure} 
-                    />
-                ))} 
-                {/* <ExpenseDetails
-                    title = {props.items[0].title}
-                    amount = {props.items[0].amount}
-                    date = {props.items[0].date}
-                />
-                <ExpenseDetails
-                    title = {props.items[1].title}
-                    amount = {props.items[1].amount}
-                    date = {props.items[1].date}
-                />
-                <ExpenseDetails
-                    title = {props.items[2].title}
-                    amount = {props.items[2].amount}
-                    date = {props.items[2].date}
-                />
-                <ExpenseDetails
-                    title = {props.items[3].title}
-                    amount = {props.items[3].amount}
-                    date = {props.items[3].date}
-                /> */}
+                />                
+                {filterExpenses.map((expense) => (
+                        <ExpenseDetails
+                            key={expense.id}
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date}
+                            // locationOfExpenditure={expense.locationOfExpenditure} 
+                        />
+                    ))} 
+
             </Card>
         </div>
   
